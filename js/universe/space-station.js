8a14169598068db48b64b445b3f7f969118ec2d8
Tapio Vierros <tapiovierros@gmail.com>
2012-03-18 11:27:52 +0200

    Started creating shops to space stations.

--- js/universe/space-station.js
+++ js/universe/space-station.js
@@ -1,4 +1,37 @@
 
+var stationTileProtos = {
+	space: new ut.Tile(),
+	wall: new ut.Tile("#", 100, 100, 100, 20, 20, 20),
+	floor: new ut.Tile(".", 140, 140, 140, 20, 20, 20)
+};
+stationTileProtos.wall.desc = "Wall";
+stationTileProtos.wall.blocks = true;
+stationTileProtos.floor.desc = "Floor";
+
+function createBox(buf, x, y, w, h, doorx, doory) {
+	for (var i = x; i <= x+w; ++i) {
+		buf[y][i] = stationTileProtos.wall;
+		buf[y+h][i] = stationTileProtos.wall;
+	}
+	for (var j = y+1; j <= y+h-1; ++j) {
+		buf[j][x] = stationTileProtos.wall;
+		buf[j][x+w] = stationTileProtos.wall;
+	}
+	if (doorx !== undefined && doory !== undefined)
+		buf[doory][doorx] = stationTileProtos.floor;
+}
+
+function createShop(buf, x, y, name, type) {
+	var shopw = 10, shoph = 10;
+	var hw = (shopw/2)|0, hh = (shoph/2)|0;
+	createBox(buf, x-hw, y-hh, shopw, shoph, x, y + hh);
+	var startx = x - Math.ceil(name.length/2), i;
+	for (i = 0; i < name.length; ++i)
+		buf[y-hh+1][startx+i] = new ut.Tile(name[i], 255, 255, 255);
+	for (i = x-hw; i <= x+hw; ++i)
+		buf[y-hh+2][i] = stationTileProtos.wall;
+}
+
 function SpaceStation(x, y, neighbours) {
 	this.size = 64;
 	this.type = "station";
@@ -18,15 +51,6 @@ function SpaceStation(x, y, neighbours) {
 	for (i = 0; i < this.size; ++i)
 		buffer[i] = new Array(this.size);
 
-	var tileProtos = {
-		space: new ut.Tile(),
-		wall: new ut.Tile("#", 100, 100, 100, 20, 20, 20),
-		floor: new ut.Tile(".", 140, 140, 140, 20, 20, 20)
-	};
-	tileProtos.wall.desc = "Wall";
-	tileProtos.wall.blocks = true;
-	tileProtos.floor.desc = "Floor";
-
 	var radius = (hsize*0.66)|0;
 	for (j = 0; j < this.size; ++j) {
 		for (i = 0; i < this.size; ++i) {
@@ -35,9 +59,20 @@ function SpaceStation(x, y, neighbours) {
 				if (rand(0, 3, rng) === 0) {
 					var star = rand(50, 200, rng);
 					buffer[j][i] = new ut.Tile("·", star, star, star);
-				} else buffer [j][i] = tileProtos.space;
-			} else if (d < radius) buffer[j][i] = clone(tileProtos.floor);
-			else buffer[j][i] = clone(tileProtos.wall);
+				} else buffer [j][i] = stationTileProtos.space;
+			} else if (d < radius) buffer[j][i] = clone(stationTileProtos.floor);
+			else buffer[j][i] = clone(stationTileProtos.wall);
+		}
+	}
+
+	var shopProtos = [
+		{ name: "Upgrades", type: "upgrade" },
+		{ name: "Laboratory", type: "laboratory" }
+	];
+	var shopPos = [ {x:0,y:-1}, {x:-1,y:0}, {x:1,y:0}, {x:0,y:1} ];
+	for (i = 0; i < shopPos.length; ++i) {
+		if (true || rand(0,2,rng) === 0) {
+			createShop(buffer, shopPos[i].x*13+hsize, shopPos[i].y*13+hsize, "Shop", "shop");
 		}
 	}
 
